4f9f7f498994a1670c05b94dbd8eaeee2bceb864
Dom Crayford <domcrayford@gmail.com>
2013-11-27 19:36:56 +0000

    Initial commit of plex API ugliier.

---
+++ plex-crawl.py
@@ -0,0 +1,20 @@
+from flask import Flask, make_response, render_template, Response
+import requests
+import xml4h
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/<path:url>')
+def pretty_plex(url=''):
+    plex = requests.get("http://127.0.0.1:32400/" + url)
+
+    if 'text/xml' in plex.headers['content-type']:
+        document = xml4h.parse(plex.text).children[0] # Grab the MediaContainer
+        return make_response(render_template('api.html', document=document), plex.status_code)
+    else:
+        return Response(plex.content, mimetype=plex.headers["content-type"])
+
+if __name__ == '__main__':
+    app.run(debug=True)
