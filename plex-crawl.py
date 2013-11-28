bed764bd5492c97514dde55c400467b1df32e150
Dom Crayford <domcrayford@gmail.com>
2013-11-28 13:41:09 +0000

    Add some info and formatting

--- plex-crawl.py
+++ plex-crawl.py
@@ -1,13 +1,38 @@
 from flask import Flask, make_response, render_template, Response
 import requests
+import time
 import xml4h
 
 app = Flask(__name__)
 
+def format_seconds(total_seconds):
+    hours = int(total_seconds / (60 * 60))
+    minutes = int(total_seconds / 60) - (hours * 60)
+    seconds = total_seconds - (minutes * 60) - (hours * 60 * 60)
+    return "%.2d:%.2d:%.2d" % (hours, minutes, seconds)
+
 
 def pretty_print(xml_node):
+    links = ["key", "parentKey"]
+    images = ["art", "thumb", "parentThumb", "banner"]
+    durations = ["duration"]
+    dates = ["updatedAt", "addedAt"]
+
     def make_attribute(key, value):
-        return { "key": key, "value": value }
+        link = info = None
+        preview = False
+        if key in links:
+            link = value
+        elif key in images:
+            link = value
+            preview = True
+
+        if key in durations:
+            info = format_seconds(int(value) / 1000)
+        elif key in dates:
+            info = time.ctime(int(value))
+
+        return { "key": key, "value": value, "link": link, "preview": preview, "info": info}
 
     try:
         name = xml_node.name
