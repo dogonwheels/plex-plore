b61316a8f54d865c383c2b71a40251eadc5ab7a5
Dom Crayford <domcrayford@gmail.com>
2013-11-28 13:16:47 +0000

    Start move node building logic into views rather than template

--- plex-crawl.py
+++ plex-crawl.py
@@ -5,6 +5,26 @@ import xml4h
 app = Flask(__name__)
 
 
+def pretty_print(xml_node):
+    def make_attribute(key, value):
+        return { "key": key, "value": value }
+
+    try:
+        name = xml_node.name
+    except AttributeError:
+        name = "Document"
+
+    if xml_node.attributes:
+        node_attributes = xml_node.attrs.items()
+    else:
+        node_attributes = []
+
+    attributes = [ make_attribute(key, value) for (key, value) in node_attributes]
+    children = [pretty_print(child) for child in xml_node.children()]
+
+    return { "name": name, "attributes": attributes, "children": children }
+
+
 @app.route('/')
 @app.route('/<path:url>')
 def pretty_plex(url=''):
@@ -12,7 +32,9 @@ def pretty_plex(url=''):
 
     if 'text/xml' in plex.headers['content-type']:
         document = xml4h.parse(plex.text.encode('utf-8'))
-        return make_response(render_template('api.html', document=document), plex.status_code)
+
+        # Parse document into pretty printed node and attributes
+        return make_response(render_template('api.html', document=pretty_print(document.children[0])), plex.status_code)
     else:
         return Response(plex.content, mimetype=plex.headers["content-type"])
 
